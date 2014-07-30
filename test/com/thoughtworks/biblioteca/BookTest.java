4a5080ef2e2ae118171d3e08d1986f3086619f71
kristinebrown10 <kristinebrown10@gmail>
2014-07-30 10:27:49 -0500

    use assert / containsString for testing book detail printing

--- test/com/thoughtworks/biblioteca/BookTest.java
+++ test/com/thoughtworks/biblioteca/BookTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.biblioteca;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,7 +9,9 @@ public class BookTest {
     @Test
     public void shouldGetBookDetails(){
         Book book = new Book("Book Name", "Book Author", "2000");
-        assertEquals("Book Name                      Book Author                    2000", book.getDetails());
+        assertThat(book.getDetails(), CoreMatchers.containsString("Book Name"));
+        assertThat(book.getDetails(), CoreMatchers.containsString("Book Author"));
+        assertThat(book.getDetails(), CoreMatchers.containsString("2000"));
     }
 
     @Test
@@ -16,6 +19,8 @@ public class BookTest {
         Book book = new Book("This is a really really really really really long book title!",
                 "Alex Jablonskiiiiiiiiiiiiiiiiiiiiii",
                 "2000");
-        assertEquals("This is a really really rea... Alex Jablonskiiiiiiiiiiiiii... 2000", book.getDetails());
+        assertThat(book.getDetails(), CoreMatchers.containsString("This is a really really rea..."));
+        assertThat(book.getDetails(), CoreMatchers.containsString("Alex Jablonskiiiiiiiiiiiiii..."));
+        assertThat(book.getDetails(), CoreMatchers.containsString("2000"));
     }
 }
